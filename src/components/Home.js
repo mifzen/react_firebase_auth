605060c79ab46cc1fd787e390963f56ff3aaf593
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2023-01-12 12:26:17 +0700

    add button to communicate with netlify function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from "react";
+
 const Home = (props) => {
+  const { email, logOut } = props;
+  const [value, setValue] = useState(0);
+  const handleFetch = async () => {
+    const response = await fetch("/.netlify/functions/increment", {
+      method: "POST",
+      body: JSON.stringify({ value }),
+    });
+    const data = await response.json();
+    setValue(data);
+  };
+
   return (
     <div>
       <p>
-        hello <b>{props.email}</b>
+        hello <b>{email}</b>
       </p>
-      <button onClick={props.logOut}>Logout</button>
+      <button onClick={logOut}>Logout</button>
+      <p>{value}</p>
+      <button onClick={handleFetch}>increment</button>
     </div>
   );
 };
